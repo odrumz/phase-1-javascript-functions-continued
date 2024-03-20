5ae02b96a1d32ebaa09732afa7f9d9a46839c253
DerrickOngoma <Odrumz64@gmail.com>
2024-03-20 12:56:33 +0300

    basics of function declaration

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun()
+function mondayWork(work = "go to the office"){
+    return `This Monday, I will ${work}.`
+}
+
+mondayWork()
+
+function wrapAdjective(wrap = "*"){
+    return function (adjective = "a hard worker"){
+    return `You are ${wrap}${adjective}${wrap}!`}
+}
